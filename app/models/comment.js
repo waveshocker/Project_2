41855e08b635aef9e77254d4cee76cb2d3a1ed73
ibrahem rayan <ibrahemrayan@ibrahems-MacBook-Pro.local>
2020-05-06 01:01:10 -0400

    login and sign up

---
+++ app/models/comment.js
@@ -0,0 +1,30 @@
+// module.exports = function(sequelize, DataTypes) {
+//     var Comment = sequelize.define("Comment", {      
+//       comment: {
+//           type: DataTypes.STRING,
+//           allowNull: false,
+//           validate: {
+//               len: [1]
+//           } 
+//       }
+//     }, {
+//         freezeTableName: true
+//     });
+  
+//     Comment.associate = function(models) {
+//       // We're saying that a comment belongs to a bikerack      
+//       Comment.belongsTo(models.Bikerack, {
+//         foreignKey: {
+//           allowNull: false
+//         }
+//       });
+
+//       Comment.belongsTo(models.User, {
+//         foreignKey: {
+//           allowNull: false
+//         }
+//       })
+//     };
+  
+//     return Comment;
+//   };
--- server.js
+++ server.js
@@ -1,30 +1,109 @@
 // Requiring necessary npm packages
-var express = require("express");
-var session = require("express-session");
+var express = require('express')
+var app = express()
+var passport = require('passport')
+var session = require('express-session')
+var bodyParser = require('body-parser')
+// var env = require('dotenv').load()
+var exphbs = require('express-handlebars')
+// var session = require("express-session");
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+// var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
-var db = require("./models");
+// var PORT = process.env.PORT || 8080;
+// var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-var app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+
+
+// var env = require('dotenv').load();
+
+
+//For BodyParser
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+// For Passport
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+})); // session secret
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); // persistent login sessions
 
-// Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
 
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  });
+//For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+
+app.get('/', function(req, res) {
+
+  res.send('Welcome to Passport with Sequelize');
+
 });
+
+//Models
+var models = require("./app/models");
+
+//Routes
+
+var authRoute = require('./app/routes/auth.js')(app);
+
+
+//load passport strategies
+
+require('./app/config/passport/passport.js')(passport, models.user);
+
+
+//Sync Database
+
+models.sequelize.sync().then(function() {
+
+  console.log('Nice! Database looks fine')
+
+
+}).catch(function(err) {
+
+  console.log(err, "Something went wrong with the Database Update!")
+
+});
+
+
+app.listen(5000, function(err) {
+
+  if (!err)
+
+      console.log("Site is live");
+       
+  else console.log(err)
+
+});
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static("public"));
+// // We need to use sessions to keep track of our user's login status
+// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// // Requiring our routes
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+
+// // Syncing our database and logging a message to the user upon success
+// db.sequelize.sync().then(function() {
+//   app.listen(PORT, function() {
+//     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+//   });
+// });
