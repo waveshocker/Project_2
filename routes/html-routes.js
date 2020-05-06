50f8e271f1780dfd7651b76f6a7e35cad0a8b758
Joel Clifford <joel.clifford@icloud.com>
2020-05-05 21:26:28 -0400

    changed default page to login and added basic google maps implementation

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/home");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/login", function(req, res) {
