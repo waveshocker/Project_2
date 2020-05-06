85f5bee56aed54395eddd1b3cb502699f3627984
ibrahem rayan <ibrahemrayan@ibrahems-MacBook-Pro.local>
2020-05-06 12:13:09 -0400

    resolved conflict

--- server.js
+++ server.js
@@ -6,18 +6,14 @@ var session = require('express-session')
 var bodyParser = require('body-parser')
 // var env = require('dotenv').load()
 var exphbs = require('express-handlebars')
-// var session = require("express-session");
 // Requiring passport as we've configured it
 // var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-// var PORT = process.env.PORT || 8080;
-// var db = require("./models");
-
-// Creating express app and configuring middleware needed for authentication
+var PORT = process.env.PORT || 8080;
+var db = require("./app/models");
 
 
-// var env = require('dotenv').load();
 
 
 //For BodyParser
@@ -89,21 +85,23 @@ app.listen(5000, function(err) {
 
 });
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static("public"));
-// // We need to use sessions to keep track of our user's login status
-// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// // Requiring our routes
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-// // Syncing our database and logging a message to the user upon success
-// db.sequelize.sync().then(function() {
-//   app.listen(PORT, function() {
-//     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-//   });
-// });
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
+});
+
+
