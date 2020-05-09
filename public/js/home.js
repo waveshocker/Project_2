be8a548b17e6056a379e114bdb3ec0a3cb66146a
unknown <binhu10@gmail.com>
2020-05-08 21:22:40 -0400

    take out test code

--- public/js/home.js
+++ public/js/home.js
@@ -2,17 +2,8 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
-  
-  var sampleLocation = {
-    longitude: -79.3811,
-    latitude: 43.6591
-  };
+    $(".member-name").text(data.email);  
 
-  $.get("/api/search_results", sampleLocation)
-    .then(function(data) {
-      console.log(data)
-    })  
   });
 });
 
@@ -100,3 +91,14 @@ function initMap() {
     map.fitBounds(bounds);
   });
 }
+
+//Sample on how to search by longitude and latitude
+var sampleLocation = {
+  longitude: -79.3811,
+  latitude: 43.6591
+};
+
+$.get("/api/search_results", sampleLocation)
+  .then(function(data) {
+    console.log(data)
+  })  
