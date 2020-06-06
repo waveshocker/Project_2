9c6ab2854f8e3bdd4ead41d4ddf254eae6e9ec74
unknown <binhu10@gmail.com>
2020-06-06 17:33:33 -0400

    remove unnecessary code

--- models/comment.js
+++ models/comment.js
@@ -1,30 +1,32 @@
-module.exports = function(sequelize, DataTypes) {
-    var Comment = sequelize.define("Comment", {      
-      comment: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-              len: [1]
-          } 
-      }
-    }, {
-        freezeTableName: true
-    });
+// module.exports = function(sequelize, DataTypes) {
+//     var Comment = sequelize.define("Comment", {      
+//       comment: {
+//           type: DataTypes.STRING,
+//           allowNull: false,
+//           validate: {
+//               len: [1]
+//           } 
+//       }
+//     }, {
+//         freezeTableName: true
+//     });
   
-    Comment.associate = function(models) {
-      // We're saying that a comment belongs to a bikerack      
-      Comment.belongsTo(models.Bikerack, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
+//     Comment.associate = function(models) {
+//       // We're saying that a comment belongs to a bikerack      
+//       Comment.belongsTo(models.Bikerack, {
+//         foreignKey: {
+//           allowNull: false
+//         }
+//       });
 
-      Comment.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      })
-    };
+//       Comment.belongsTo(models.User, {
+//         foreignKey: {
+//           allowNull: false
+//         }
+//       })
+//     };
   
-    return Comment;
-  };
+//     return Comment;
+//   };
+
+module.exports=function(e,l){var o=e.define("Comment",{comment:{type:l.STRING,allowNull:!1,validate:{len:[1]}}},{freezeTableName:!0});return o.associate=function(e){o.belongsTo(e.Bikerack,{foreignKey:{allowNull:!1}}),o.belongsTo(e.User,{foreignKey:{allowNull:!1}})},o};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
+var compression = require("compression");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
@@ -22,6 +23,7 @@ app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(compression())
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
@@ -30,76 +32,76 @@ require("./routes/bike-routes.js")(app);
 
 //code set up to pull from Open Data API
 // promise to retrieve the package
-const getPackage = new Promise((resolve, reject) => {
-  https.get(`https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show?id=${packageName}`, (response) => {
-      let dataChunks = [];
-      response
-          .on("data", (chunk) => {
-              dataChunks.push(chunk)
-          })
-          .on("end", () => {
-              let data = Buffer.concat(dataChunks)
-              resolve(JSON.parse(data.toString())["result"])
-          })
-          .on("error", (error) => {
-              reject(error)
-          })
-  });
-  });
+// const getPackage = new Promise((resolve, reject) => {
+//   https.get(`https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show?id=${packageName}`, (response) => {
+//       let dataChunks = [];
+//       response
+//           .on("data", (chunk) => {
+//               dataChunks.push(chunk)
+//           })
+//           .on("end", () => {
+//               let data = Buffer.concat(dataChunks)
+//               resolve(JSON.parse(data.toString())["result"])
+//           })
+//           .on("error", (error) => {
+//               reject(error)
+//           })
+//   });
+//   });
   
-  const getDatastoreResource = resource => new Promise((resolve, reject) => {
-  https.get(`https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/datastore_search?id=${resource["id"]}`, (response) => {
-      let dataChunks = [];
-      response
-          .on("data", (chunk) => {
-              dataChunks.push(chunk)
-          })
-          .on("end", () => {
-              let data = Buffer.concat(dataChunks)
-              resolve(JSON.parse(data.toString())["result"]["records"])
-          })
-          .on("error", (error) => {
-              reject(error)
-          })
-  })
-  });
+//   const getDatastoreResource = resource => new Promise((resolve, reject) => {
+//   https.get(`https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/datastore_search?id=${resource["id"]}`, (response) => {
+//       let dataChunks = [];
+//       response
+//           .on("data", (chunk) => {
+//               dataChunks.push(chunk)
+//           })
+//           .on("end", () => {
+//               let data = Buffer.concat(dataChunks)
+//               resolve(JSON.parse(data.toString())["result"]["records"])
+//           })
+//           .on("error", (error) => {
+//               reject(error)
+//           })
+//   })
+//   });
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({force: false}).then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
 
 //pulling from open Data API
-getPackage.then(package => {
+// getPackage.then(package => {
 
-  // get the datastore resources for the package
-  let datastoreResources = package["resources"].filter(r => r.datastore_active);
+//   // get the datastore resources for the package
+//   let datastoreResources = package["resources"].filter(r => r.datastore_active);
   
-  // retrieve the first datastore resource as an example
-  getDatastoreResource(datastoreResources[0])
-      .then(resource => {
-          for(i=0; i < resource.length; i++) {
-              db.Bikerack.findOrCreate({
-                  where: {
-                  _id: resource[i]._id,
-                  address: resource[i].ADDRESS_FULL,
-                  bike_capacity: resource[i].BICYCLE_CAPACITY,
-                  longitude: resource[i].LONGITUDE,
-                  latitude: resource[i].LATITUDE
-              }, defaults: {
-                  _id: resource[i]._id,
-                  address: resource[i].ADDRESS_FULL,
-                  bike_capacity: resource[i].BICYCLE_CAPACITY,
-                  longitude: resource[i].LONGITUDE,
-                  latitude: resource[i].LATITUDE
-              }});
-          }
-      })
-      .catch(error => {
-          console.error(error);
-      })
-  }).catch(error => {
-  console.error(error);
-  })
+//   // retrieve the first datastore resource as an example
+//   getDatastoreResource(datastoreResources[0])
+//       .then(resource => {
+//           for(i=0; i < resource.length; i++) {
+//               db.Bikerack.findOrCreate({
+//                   where: {
+//                   _id: resource[i]._id,
+//                   address: resource[i].ADDRESS_FULL,
+//                   bike_capacity: resource[i].BICYCLE_CAPACITY,
+//                   longitude: resource[i].LONGITUDE,
+//                   latitude: resource[i].LATITUDE
+//               }, defaults: {
+//                   _id: resource[i]._id,
+//                   address: resource[i].ADDRESS_FULL,
+//                   bike_capacity: resource[i].BICYCLE_CAPACITY,
+//                   longitude: resource[i].LONGITUDE,
+//                   latitude: resource[i].LATITUDE
+//               }});
+//           }
+//       })
+//       .catch(error => {
+//           console.error(error);
+//       })
+//   }).catch(error => {
+//   console.error(error);
+//   })
